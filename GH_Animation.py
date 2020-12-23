1a89b0aa706406de2bd8023ec9d76a7ffcbc9ec2
hira113 <75287369+hira113@users.noreply.github.com>
2020-12-23 14:00:56 +0900

    Update GH_Animation.py
    インスタンスのコンポーネント内では動いているMoveobj関数が、このアニメーションのコンポーネント内では、実行するとエラーになってしまいます。エラーの原因が分かりません、、、

--- GH_Animation.py
+++ GH_Animation.py
@@ -1,16 +1,23 @@
 # coding: utf-8
 import rhinoscriptsyntax as rs
 
+
 print(circle_packs)
 circle_num = len(circle_packs)
+print(circle_num)
+
+
 
 for i in range(circle_num):
     my_circle = circle_packs[i]
     
-    for j in ragen(circle_num):
+    for j in range(circle_num):
         you_circle = circle_packs[j]
+        
         
         if my_circle == you_circle:
             continue
+        move = my_circle.Moveobj(you_circle)
         
-        #-TODO この続きを書く
+        
+out_circles = move_objs
